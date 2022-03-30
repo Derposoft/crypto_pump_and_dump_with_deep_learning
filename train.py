ea4b9369733b9ac71052db96ab90dee1b4668885
anujvasil <avasil@usc.edu>
2022-03-29 21:37:53 -0700

    fixed training loop

--- train.py
+++ train.py
@@ -4,12 +4,14 @@ from models.conv_lstm import *
 import pandas as pd
 
 
-def train(model, iterator, labels, opt, criterion):
+def train(model, iterator, opt, criterion):
     epoch_loss = 0
-    for batch, label in iterator, labels:
+    n_seq = 2
+    for batch in iterator:
         opt.zero_grad()
-        pred = model(batch)
-        loss = criterion(pred, label)
+        x = batch[:, :-1].chunk(n_seq)
+        pred = model(torch.stack(list(x), dim=0))
+        loss = criterion(pred[:, :, -1], batch[:, -1])
         loss.backward()
         opt.step()
 
@@ -20,23 +22,23 @@ def train(model, iterator, labels, opt, criterion):
 
 def main():
     data = pd.read_csv('data/features_5S.csv.gz', compression='gzip')
-    labels = pd.read_csv('data/pump_telegram.csv')
+    data = data.drop(columns=['symbol', 'date']).to_numpy()
+    
 
     EMBEDDING_SIZE = 64
     N_LAYERS = 1
     LEARNING_RATE = .01
     N_EPOCHS = 10
-    KERNEL_SIZE = 2
+    KERNEL_SIZE = 1
     BATCH_SIZE = 10
 
     train_loader = DataLoader(data, batch_size = BATCH_SIZE)
-    labels_loader = DataLoader(labels, batch_size=1)
 
-    model = ConvLSTM(16, KERNEL_SIZE, EMBEDDING_SIZE, N_LAYERS)
-    opt = torch.optim.Adam(model.params)
+    model = ConvLSTM(13, KERNEL_SIZE, EMBEDDING_SIZE, N_LAYERS)
+    opt = torch.optim.Adam(model.parameters())
     criterion = torch.nn.BCEWithLogitsLoss()
 
     for epoch in range(N_EPOCHS):
         print('Epoch: ' + str(epoch))
-        train_loss = train(model, train_loader, labels_loader, opt, criterion)
+        train_loss = train(model, train_loader, opt, criterion)
         print('ELoss: ' + str(train_loss))
