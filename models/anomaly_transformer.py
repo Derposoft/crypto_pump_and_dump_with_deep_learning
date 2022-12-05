03914beeea79902b201ee58c23a55d0583a0f546
Girish Mahajan <type591234@gmail.com>
2022-12-04 22:14:58 -0800

    added back old AT to test

--- models/anomaly_transformer.py
+++ models/anomaly_transformer.py
@@ -281,3 +281,91 @@ class AnomalyTransfomerIntermediate(nn.Module):
         P_list = [P.detach() for P in self.P_layers]
         return F.mse_loss(preds, y) + self.lambda_*torch.mean(torch.abs(self.association_discrepancy(P_list, S_list)))
 
+class AnomalyTransformerOld(nn.Module):
+    def __init__(self, N, d_model, layers, lambda_, device):
+        super().__init__()
+        self.N = N
+        self.d_model = d_model
+
+        self.blocks = nn.ModuleList(
+            [AnomalyTransformerBlock(self.N, self.d_model, device) for _ in range(layers)]
+        )
+        self.output = None
+        self.lambda_ = lambda_
+        self.classifier = nn.Linear(d_model, 1)
+
+        self.P_layers = []
+        self.S_layers = []
+
+    def forward(self, x):
+        self.P_layers = []
+        self.S_layers = []
+        for block in self.blocks:
+            x = block(x)
+            self.P_layers.append(block.attention.P)
+            self.S_layers.append(block.attention.S)
+        x = self.classifier(x)
+        return x.squeeze(2)
+
+    def layer_association_discrepancy(self, Pl, Sl):
+        rowwise_kl = lambda row: (
+            F.kl_div(Pl[row, :], Sl[row, :]) + F.kl_div(Sl[row, :], Pl[row, :])
+        )
+        ad_vector = torch.concat(
+            [rowwise_kl(row).unsqueeze(0) for row in range(Pl.shape[0])]
+        )
+        return ad_vector
+
+    def association_discrepancy(self, P_list, S_list):
+
+        return (1 / len(P_list)) * sum(
+            [
+                self.layer_association_discrepancy(P, S)
+                for P, S in zip(P_list, S_list)
+            ]
+        )
+
+    def loss_function(self, x_hat, P_list, S_list, lambda_, x):
+        # frob_norm = torch.linalg.matrix_norm(x_hat - x, ord="fro")
+        mse_loss = F.mse_loss(x_hat, x)
+        return mse_loss - (
+            lambda_
+            # * torch.linalg.norm(self.association_discrepancy(P_list, S_list), ord=1)
+            * torch.mean(torch.abs(self.association_discrepancy(P_list, S_list)))
+        )
+
+    def min_loss(self, preds, y):
+        P_list = self.P_layers
+        S_list = [S.detach() for S in self.S_layers]
+        lambda_ = -self.lambda_
+        return self.loss_function(preds, P_list, S_list, lambda_, y)
+
+    def max_loss(self, preds, y):
+        P_list = [P.detach() for P in self.P_layers]
+        S_list = self.S_layers
+        lambda_ = self.lambda_
+        return self.loss_function(preds, P_list, S_list, lambda_, y)
+
+    def loss_fn(self, preds, y):
+        loss = self.min_loss(preds, y)
+        loss.backward(retain_graph=True)
+        loss = self.max_loss(preds, y)
+        return loss
+
+    def anomaly_score(self, preds, y):
+        """
+        Not used. Useful in unsupervsied setting but unecessary for our purpose
+        """
+        ad = F.softmax(
+            -self.association_discrepancy(self.P_layers, self.S_layers, y), dim=0
+        )
+        assert ad.shape[0] == self.N
+        norm = torch.tensor(
+            [
+                torch.linalg.norm(y[i, :] - preds[i, :], ord=2)
+                for i in range(self.N)
+            ]
+        )
+        assert norm.shape[0] == self.N
+        score = torch.mul(ad, norm)
+        return score
